dde24f3774b5352be1e78fc7777109d3a534a2da
jlvnunez <jlvnunez001@gmail.com>
2026-01-17 15:30:20 -0300

    exercicio 20 js update

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ let res =document.querySelector("#saida")
 
 let estacao =""
 
-if((mes === "setembro" && dia >= 22) || mes === "outubro" || mes === "Novembro" || (mes === "dezembro" && dia < 21)){
-    estacao = "Primavera"
-}else if((mes === "dezembro" && dia >= 21 )|| mes ==="janeiro" || mes ==="fevereiro" || (mes ==="marco && dia <=21")){
-    estacao = "Verão"
-}else if((mes === "marco" && dia >= 20) || mes === "abril" || mes === "maio" || (mes === "junho" && dia < 21)){
-    estacao = "Outono"
-}else if((mes === "junho" && dia >= 21) || mes === "julho" || mes === "agosto" || (mes === "setembro" && dia < 22)) {
-    estacao = "Inverno}"
-}else{
-    estacao = "Mes invalido ,Digite novamenta!"
-}
+if ((mes === "setembro" && dia >= 22) || mes === "outubro" || mes === "novembro" || (mes === "dezembro" && dia < 21)) {
+        estacao = "Primavera";
+    } else if ((mes === "dezembro" && dia >= 21) || mes === "janeiro" || mes === "fevereiro" || (mes === "março" || mes === "marco" && dia < 20)) {
+        estacao = "Verão";
+    } else if ((mes === "março" || mes === "marco" && dia >= 20) || mes === "abril" || mes === "maio" || (mes === "junho" && dia < 21)) {
+        estacao = "Outono";
+    } else if ((mes === "junho" && dia >= 21) || mes === "julho" || mes === "agosto" || (mes === "setembro" && dia < 22)) {
+        estacao = "Inverno";
+    } else {
+        estacao = "Dados inválidos. Verifique o dia e o mês!";
+    }
 res.innerHTML= `<p>No dia <strong>${dia}</strong> do Mês de <strong>${mes}</strong>,estamos na estação de <mark><strong>${estacao}</strong></mark></p>`
 
 }
