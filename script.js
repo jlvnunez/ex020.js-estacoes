9bb34a71920446d9ae4e2990a418e8f30d05ad17
jlvnunez <jlvnunez001@gmail.com>
2026-01-17 20:22:21 -0300

    ex 20 js update

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 function iniciar(){
 let dia = parseInt(prompt("Digite um DIA DO MÊS"))
 let mesEntrada = prompt("Digite um MÊS DO ANO")
+let res =document.querySelector("#saida")
+
+// 1. Verificação de dia inválido (menor que 1 ou maior que 31)
+    if (isNaN(dia) || dia < 1 || dia > 31) {
+        res.innerHTML = "<p><strong style='color:red;'>Erro: Dia inválido! Digite um valor entre 1 e 31.</strong></p>";
+        return; // O 'return' para a execução da função aqui mesmo
+    }
+
+    if (!mesEntrada || !isNaN(Number(mesEntrada))) {
+        res.innerHTML = "<p style='color:red;'><strong>Erro:</strong> Digite o nome do mês por extenso (não use números).</p>";
+        return;
+    }
 
 if (!mesEntrada) return; // Evita erro se o usuário cancelar o prompt
     let mes = mesEntrada.toLowerCase().trim();
 
-    let res =document.querySelector("#saida")
+   
 
 let estacao =""
 
@@ -18,11 +30,12 @@ if ((mes === "setembro" && dia >= 22) || mes === "outubro" || mes === "novembro"
     } else if ((mes === "junho" && dia >= 21) || mes === "julho" || mes === "agosto" || (mes === "setembro" && dia < 22)) {
         estacao = "Inverno";
     } else {
-        estacao = "Dados inválidos. Verifique o dia e o mês!";
+       // estacao = "<strong style = color:red Dados inválidos. Verifique o dia e o mês!></strong>";
     }
 res.innerHTML= `<p>No dia <strong>${dia}</strong> do Mês de <strong>${mes}</strong>,estamos na estação de <mark><strong>${estacao}</strong></mark></p>`
 
 }
+
 
 function limpar(){
     let res = document.querySelector("#saida")
